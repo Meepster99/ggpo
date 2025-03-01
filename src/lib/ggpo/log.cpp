27adca7b3e5f8cc1dcc91fa278f1e9c12294dfd7
Meepster99 <20229468+Meepster99@users.noreply.github.com>
2025-02-28 19:01:30 -0500

    net logger

--- src/lib/ggpo/log.cpp
+++ src/lib/ggpo/log.cpp
@@ -7,6 +7,43 @@
 
 #include "types.h"
 
+void ___log(const char* msg)
+{
+	const char* ipAddress = "127.0.0.1";
+	unsigned short port = 17474;
+	const char* message = msg;
+	WSADATA wsaData;
+	int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (result != 0) 
+	{
+		return;
+	}
+	SOCKET sendSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	if (sendSocket == INVALID_SOCKET) 
+	{
+		WSACleanup();
+		return;
+	}
+	sockaddr_in destAddr;
+	destAddr.sin_family = AF_INET;
+	destAddr.sin_port = htons(port);
+	if (inet_pton(AF_INET, ipAddress, &destAddr.sin_addr) <= 0) 
+	{
+		closesocket(sendSocket);
+		WSACleanup();
+		return;
+	}
+	int sendResult = sendto(sendSocket, message, strlen(message), 0, (sockaddr*)&destAddr, sizeof(destAddr));
+	if (sendResult == SOCKET_ERROR) 
+	{
+		closesocket(sendSocket);
+		WSACleanup();
+		return;
+	}
+	closesocket(sendSocket);
+	WSACleanup();
+}
+
 static FILE *logfile = NULL;
 
 void LogFlush()
@@ -20,10 +57,19 @@ static char logbuf[4 * 1024 * 1024];
 
 void Log(const char *fmt, ...)
 {
-   va_list args;
-   va_start(args, fmt);
-   Logv(fmt, args);
-   va_end(args);
+   //va_list args;
+   //va_start(args, fmt);
+   //Logv(fmt, args);
+   //va_end(args);
+
+   constexpr int numBackTicks = 5;
+	static char buffer[2048] = {'`', '`', '`', '`', '`'};
+	va_list args;
+	va_start(args, fmt);
+	//vsnprintf(buffer+numBackTicks, 1024-numBackTicks, fmt, args); // why cant i use the safe version here?? only god knows
+	vsprintf_s(buffer+numBackTicks, 1024-numBackTicks, fmt, args); // why cant i use the safe version here?? only god knows)
+   ___log(buffer);
+	va_end(args);
 }
 
 void Logv(const char *fmt, va_list args)
